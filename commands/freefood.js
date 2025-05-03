4cb0468f8fc9f2f03a499fea1027cada4e77fc68
Neb <nebulaskies00@gmail.com>
2025-05-03 17:31:48 -0500

    First working iteration of the free food command

--- commands/freefood.js
+++ commands/freefood.js
@@ -2,46 +2,49 @@ module.exports = {
     name: 'freefood',
     description: 'Lists all free food events on OU campus',
     feature: 'freefood',
-    execute(message, args, client) {
-        imageUrls = ''
-        this.fetchEngage().then(result => {
-            imageUrls += result
-        })
+    async execute(message, args, client) {
+        //Can either be an image URL or a text description as a fallback
+        let eventLinks = await this.fetchEngage()
 
-        if (imageUrls != '') {
-            return message.reply(imageUrls)
+        if (eventLinks.length === 0) {
+            return message.reply('Could not find any free food :cry:')
         }
 
-        return message.reply('Could not find any free food :cry:')
+        let reply = 'Found free food!\n'
+
+        for (const link of eventLinks) {
+            reply += link + '\n'
+        }
+
+        message.reply(reply)
     },
 
     async fetchEngage() {
         let json
-        let imageUrls = ''
+        let eventLinks = []
         try {
             const response = await fetch('https://ou.campuslabs.com/engage/api/discovery/event/search')
             if (!response.ok) {
                 throw new Error(`Response status: ${response.status}`);
             }
+            let json = await response.json()
 
-            json = await response.json()
+            for (const event of json.value) {
+                if (event.benefitNames.includes('Free Food') || this.stringContainsFood(event.description)) {
+                    eventLinks.push('https://ou.campuslabs.com/engage/event/' + event.id)
+                }
+            }
+
         } catch (error) {
             console.error(error.message)
-            return ''
         }
-
-        for (const event in json.value) {
-            console.log(event)
-            if (event.benefitNames.includes('Free Food') || stringContainsFood(event.description)) {
-                imageUrls += 'https://se-images.campuslabs.com/clink/images/' + event.imagePath +'\n'
-            }
-        }
-        return imageUrls
+        
+        return eventLinks
+        
     },
 
     stringContainsFood(str) {
         keywords = ["food", "snack", "pizza"]
-
         for (const keyword of keywords) {
             if (str.includes(keyword)) return true
         }
