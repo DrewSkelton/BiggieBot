7af99d9b0331745bafd424837eedb7329537b97f
Neb <nebulaskies00@gmail.com>
2025-05-17 19:17:33 -0500

    Rewrite of the counting feature to use a mongodb database

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,5 @@
 FROM node:alpine
 
-VOLUME /usr/src/app/data
-
 # Change the timezone to central
 RUN apk add tzdata
 RUN ln -s /usr/share/zoneinfo/US/Central /etc/localtime
@@ -13,4 +11,3 @@ COPY package*.json ./
 RUN npm install
 COPY . ./
 CMD ["node", "index.js"]
-
--- commands/setcounting.js
+++ commands/setcounting.js
@@ -1,10 +1,11 @@
 const { PermissionsBitField } = require('discord.js');
+const database = require('../utils/database.js')
 
 module.exports = {
   name: 'setcounting',
   description: 'Sets the current channel as the counting channel',
   feature: 'counting',
-  execute(message, args, client) {
+  async execute(message, args, client) {
     // Check if user has administrator permissions
     if (!message.member.permissions.has(PermissionsBitField.Flags.Administrator)) {
       return message.reply('❌ You need administrator permissions to use this command.');
@@ -17,9 +18,21 @@ module.exports = {
     }
     
     // Set the current channel as the counting channel
-    countingFeature.setCountingChannel(message.channel.id);
-    countingFeature.resetCount(); // Reset count to 0
+    const collection = await database.collection("counting")
+
+    const result = await collection.findOneAndDelete({_id: message.channel.id});
+
+    if (result == null) {
+      await collection.insertOne({
+        _id: message.channel.id,
+        userId: null,
+        count: 0
+      });
+
+      return message.reply('✅ This channel has been set as the counting channel! Start counting from 1.');
+    }
     
-    return message.reply('✅ This channel has been set as the counting channel! Start counting from 1.');
+    return message.reply('❌ This channel has been removed as the counting channel.');
+    
   },
 };
