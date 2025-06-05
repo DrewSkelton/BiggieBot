1735272233c44272daa0fa3d82aa04a28984f714
Neb <nebulaskies00@gmail.com>
2025-06-04 20:41:02 -0500

    Remove question pool

--- features/dailyQuestion.js
+++ features/dailyQuestion.js
@@ -4,36 +4,8 @@ const database = require('../utils/database.js');
 module.exports = {
   name: 'dailyQuestion',
   description: 'Posts daily questions in a specific channel',
-  
-  // Questions pool
-  questions: [
-    "Send a screenshot of your phone's screen time",
-    "When's the last time you've washed your bed's comforter/sheets?",
-    "What's your spirit animal?",
-    "What's your favorite song right now?",
-    "What is the best chicken restaurant?",
-    "How old were you when you got on social media for the first time?",
-    "Who in the group do you think would make the best dorm (same-room) roommate?",
-    "On a scale of 1-10, how much do you want to kiss Ian?",
-    "What is your dream car? (show photo)",
-    "If you had to drink the bath water of one person in this group, who would it be?",
-    "Would you move somewhere other than Oklahoma post-grad? If so, where?",
-    "If you could commit one crime and get away with it, what would it be?",
-    "What is your biggest pet peeve?",
-    "If you could play one musical instrument, what would it be?",
-  ],
 
-  async init(client) {
-    // Initialize database with questions
-    // This is only for testing
-    // (Maybe users can DM the bot questions?)
-    const collection = await database.collection('daily_question');
-    await collection.updateOne( {}, {
-      $addToSet: {
-          questions: {$each: this.questions}
-        }},
-        {upsert: true});
-    
+  async init(client) {    
     // Schedule the first daily question at 9 AM
     cron.schedule('0 9 * * *', () => {
       this.askDailyQuestion(client);
