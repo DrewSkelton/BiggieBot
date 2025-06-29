919fe94eb093298048d36e792d386d4d82c467c8
Neb <nebulaskies00@gmail.com>
2025-06-29 13:18:05 -0500

    Remove exponential notation from the calc command

--- src/commands/calc.ts
+++ src/commands/calc.ts
@@ -13,7 +13,10 @@ export const command = new SlashCommandBuilder()
 export async function execute(interaction: ChatInputCommandInteraction) {
     try {
         await interaction.reply(
-            format(evaluate(interaction.options.getString('expression')), 14)
+            // Default precision is 16. We set the precision to 14 to hide precision errors from expressions like 0.1 + 0.2.
+            // But when precision is set and notation is set to 'fixed', then the number is padded to fit the precision so
+            // 1 would become 1.00000000000000, which is why we use the auto notation with infinite exponent bounds.
+            format(evaluate(interaction.options.getString('expression')), {precision: 14, lowerExp: -Infinity, upperExp: Infinity})
         );
     }
     catch(error) {
