b3ad83da9fc6af79fee9ddcbf374e203faf94b92
Neb <nebulaskies00@gmail.com>
2025-09-15 14:07:03 -0500

    Remove religious connotations from the oracle command

--- src/commands/oracle.ts
+++ src/commands/oracle.ts
@@ -2,7 +2,7 @@ import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
 
 export const command = new SlashCommandBuilder()
     .setName('oracle')
-    .setDescription("Talk to God with Terry Davis's oracle.")
+    .setDescription("A port of the Oracle program from TempleOS.")
     .addIntegerOption(option => option
         .setName('words')
         .setDescription('The number of words to generate. Leave empty for a random amount.')
@@ -19,12 +19,12 @@ export async function execute(interaction: ChatInputCommandInteraction) {
     
     let words = interaction.options.getInteger('words')
 
-    let reply = '**God says:** '
+    let reply = `**He says:** `
 
     for (let i = 0; !words || i < words && reply.length < 2000; i++) {
         reply += dictionary[Math.floor(Math.random() * dictionary.length)] + ' '
         // If no words are specified, then there will be a 5% chance to end the loop, resulting in a logarithmic distribution of random words
-        if (!words && Math.random() < 0.05) break 
+        if (!words && Math.random() < 0.05) break
     }
 
     await interaction.reply(reply)
