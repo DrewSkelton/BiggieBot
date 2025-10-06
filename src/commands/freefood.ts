21309d6d7b06c2a36066890b45d9f17f3908fcd0
DrewSkelton <drew.skelton@ou.edu>
2025-10-06 12:15:58 -0500

    more free_food days

--- src/commands/freefood.ts
+++ src/commands/freefood.ts
@@ -30,7 +30,14 @@ export async function execute(interaction: ChatInputCommandInteraction) {
 
 async function fetchEngage(days_ahead: number) {
   const now = new Date()
+  
+  const leftDate = new Date()
 
+  let rightDate = new Date(
+    now.getFullYear(),
+    now.getMonth(),
+    now.getDate() + 1,
+  )
   //Round to the nearest next day
   const dateTo = new Date(
     now.getFullYear(),
@@ -38,36 +45,55 @@ async function fetchEngage(days_ahead: number) {
     now.getDate() + days_ahead + 1,
   )
 
-  const eventLinks = []
-  try {
-    const response = await fetch(
-      `https://ou.campuslabs.com/engage/api/discovery/event/search?endsAfter=${now.toISOString()}&startsBefore=${dateTo.toISOString()}`,
-    )
-    if (!response.ok) {
-      throw new Error(`Response status: ${response.status}`)
-    }
-    const json: any = await response.json()
 
-    for (const event of json.value) {
-      if (
-        event.benefitNames.includes("Free Food") ||
-        stringContainsFood(event.description)
-      ) {
-        const today = new Date()
-        const formattedDate = today.getDate() === today.getDate() ? "Today" : today.getDate() === today.getDate() + 1 ? "Tomorrow" : today.toLocaleDateString()
-        eventLinks.push(
-          `# [${event.name}](https://ou.campuslabs.com/engage/event/${event.id})\n` +
-            //Discord date formatters use seconds instead of milliseconds
-            `${formattedDate}, <t:${Math.floor(Date.parse(event.startsOn) / 1000)}:t>-<t:${Math.floor(Date.parse(event.endsOn) / 1000)}:t> at **${event.location}**`,
-        )
+  console.log(dateTo)
+
+  const eventLinks: string[] = []
+
+  try {
+    const responses = []
+    while (rightDate <= dateTo) {
+      const response = await fetch(
+        `https://ou.campuslabs.com/engage/api/discovery/event/search?endsAfter=${leftDate.toISOString()}&startsBefore=${rightDate.toISOString()}`,
+      )
+
+      if (!response.ok) {
+        throw new Error(`Response status: ${response.status}`)
+      }
+      const json: any = await response.json()
+      responses.push(json)
+      console.log( json )
+
+      rightDate.setDate(rightDate.getDate() + 1)
+      leftDate.setDate(leftDate.getDate() + 1)
+    }
+    
+    console.log(`https://ou.campuslabs.com/engage/api/discovery/event/search?endsAfter=${leftDate.toISOString()}&startsBefore=${dateTo.toISOString()}`)
+    
+    for (const json of responses) {
+      for (const event of json.value) {
+        if (
+          event.benefitNames.includes("Free Food") ||
+          stringContainsFood(event.description)
+        ) {
+          const today = new Date().getDate()
+          const tomorrow = new Date().getDate() + 1
+          const eventDate = new Date(event.startsOn).getDate()
+          const formattedDate = eventDate === today ? "Today" : eventDate === tomorrow ? "Tomorrow" : new Date(event.startsOn).toLocaleDateString(undefined, { weekday: 'long'})
+          eventLinks.push(
+            `# [${event.name}](https://ou.campuslabs.com/engage/event/${event.id})\n` +
+              //Discord date formatters use seconds instead of milliseconds
+              `${formattedDate}, <t:${Math.floor(Date.parse(event.startsOn) / 1000)}:t>-<t:${Math.floor(Date.parse(event.endsOn) / 1000)}:t> at **${event.location}**`,
+          )}
+        }
       }
     }
-  } catch (error) {
-    console.error(error)
-  }
+    catch (error) {
+      console.error(error)
+    }
+    return eventLinks
+  } 
 
-  return eventLinks
-}
 
 function stringContainsFood(str: string): boolean {
   const keywords = ["food", "snack", "pizza"]
