0b2a58bace6bdb8fe4b6cb8f8e6410d85a36f632
Benjamin <benjamin.wheeler00@outlook.com>
2026-08-07 15:35:07 -0500

    Add a comment explaining the schema files in the features folder requirement

--- drizzle.config.js
+++ drizzle.config.js
@@ -3,6 +3,8 @@ import { defineConfig } from "drizzle-kit"
 
 export default defineConfig({
   out: "migrations",
+  // Drizzle does not support excluding patterns, and including main causes a circular dependency
+  // Thus we must use a sub-folder
   schema: "src/features/**",
   dialect: "sqlite",
   dbCredentials: {
