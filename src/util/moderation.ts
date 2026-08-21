b138246bd37e53df3b49af45b664a2e1fa64fdfd
Benjamin <benjamin.wheeler00@outlook.com>
2026-08-21 17:57:08 -0500

    Don't delete messages if a user cannot be kicked and send a DM

--- src/util/moderation.ts
+++ src/util/moderation.ts
@@ -1,19 +1,33 @@
 import { Guild, User } from "discord.js"
 
-export async function kickAndDeleteRecentMessages(user: User, guild: Guild) {
+/// Returns true if the user was successfully kicked
+export async function kickAndDeleteRecentMessages(
+  user: User,
+  guild: Guild,
+): Promise<boolean> {
   try {
     await guild.members.kick(user)
-  } catch {}
-  for (const channel of guild.channels.cache) {
-    if (channel[1].isTextBased()) {
-      for (const message of channel[1].messages.cache) {
-        if (
-          message[1].author == user &&
-          Date.now() - message[1].createdAt.getTime() < 1000 * 60 // 1 minute
-        ) {
-          await message[1].delete()
+    // The rest of the block will not execute if the user was not kicked
+
+    for (const channel of guild.channels.cache) {
+      if (channel[1].isTextBased()) {
+        for (const message of channel[1].messages.cache) {
+          if (
+            message[1].author == user &&
+            Date.now() - message[1].createdAt.getTime() < 1000 * 60 // 1 minute
+          ) {
+            await message[1].delete()
+          }
         }
       }
     }
+
+    await user.send(
+      `You have been kicked from **${guild.name}** for suspected bot activity.`,
+    )
+
+    return true
+  } catch {
+    return false
   }
 }
